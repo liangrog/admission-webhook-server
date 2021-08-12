236b5bfc5605ea66cfe6c1c50c71618fa46e8f04
Alexandru Cojocaru <alexandru.cojocaru@crossover.com>
2021-08-12 10:15:28 +0300

    Added the Subject Alternative Name field in the generated SSL certificate. (#4)
    This field is mandatory starting with Go 1.15, which is used by Kubernetes 1.19 (https://v1-19.docs.kubernetes.io/docs/setup/release/notes/#api-change).

--- ssl.sh
+++ ssl.sh
@@ -20,5 +20,7 @@ cat ca.crt >> server.pem
 # Generate the private key for the webhook server
 openssl genrsa -out tls.key 2048
 # Generate a Certificate Signing Request (CSR) for the private key, and sign it with the private key of the CA.
+# https://security.stackexchange.com/a/190906
 openssl req -new -days $expiration -key tls.key -subj "/CN=$cn" \
-    | openssl x509 -days $expiration -req -CA ca.crt -CAkey ca.key -CAcreateserial -out tls.crt
+    | openssl x509 -days $expiration -req -CA ca.crt -CAkey ca.key -CAcreateserial -out tls.crt \
+        -extensions SAN -extfile <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=DNS:$cn"))
